33cddbbbed956b425d4799fdfd5eaddbfb9b6adf
chulleyboy <ryanmmassenburg@gmail.com>
2023-04-19 19:25:19 -0700

    added framework for new application structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const { start } = require('repl');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tourney_db');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +19,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+}
+startApolloServer(typeDefs, resolvers);
